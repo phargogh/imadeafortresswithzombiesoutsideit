d8d39f05b0c6ea9aaf3d22c9a317b57450ad63d5
Sean Francis-Lyon <seanflyon@gmail.com>
2014-12-06 12:41:56 -0800

    empty hand and card

---
+++ zomblieFortress/Assets/Hand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hand : MonoBehaviour {
+
+	public Queue<Card> cards = new Queue<Card>();
+	public static int handSize = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
