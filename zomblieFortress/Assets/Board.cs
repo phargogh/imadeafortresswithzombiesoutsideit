7adf27787041326aa0dca897c58a7d0fa463d066
Sean Francis-Lyon <seanflyon@gmail.com>
2014-12-06 02:14:57 -0800

    empty towers and walls

--- zomblieFortress/Assets/Board.cs
+++ zomblieFortress/Assets/Board.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Board : MonoBehaviour {
-	
+
+	public GameObject wallFab;
+	public GameObject towerFab;
 	static int width = 32;
 	GameObject [,] board = new GameObject[width,width];
 	List<Zombie> zombies = new List<Zombie>();
