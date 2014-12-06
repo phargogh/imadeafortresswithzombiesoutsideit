f9dd74c4e76b17ebc91cc3eba1a1b0e259da82fd
James Douglass <jamesdouglassusa@gmail.com>
2014-12-06 11:13:59 -0800

    Starting some wasteland detection

--- zomblieFortress/Assets/Board.cs
+++ zomblieFortress/Assets/Board.cs
@@ -29,4 +29,49 @@ public class Board : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void DetectWasteland () {
+		// build up an empty 2d matrix for indicating which cells are wasteland.
+		// initialize to false.
+		// TODO: use Board.widthx, Board.widthy
+		bool[,] wasteland = new bool[4, 4];
+		for (int i = 0; i < Board.widthx; i++){
+			for (int j = 0; j < Board.widthy; j++){
+				Debug.Log(wasteland[i, j]);
+			}
+		}
+
+		// start out by starting from (0, 0) and investigating the board from there.
+		//bool[,] wasteland = new bool[landscape.GetLength(0), landscape.GetLength (1)];
+
+	}
+
+	bool[,] RecurseWasteland (bool[,] landscape, Point start_point) {
+		//determine whether the start point is wasteland
+
+		for (int cardinal_dir = 0; cardinal_dir < 4; cardinal_dir++){
+
+			Point new_search_index = new Point();
+			// TODO: check the boundary cases.
+			if (cardinal_dir == 0){ // new direction is N.
+				new_search_index.x = start_point.x;
+				new_search_index.y = start_point.y - 1;
+			}
+			else if (cardinal_dir == 1) { // new direction is W
+				new_search_index.x = start_point.x - 1;
+				new_search_index.y = start_point.y;
+			}
+			else if (cardinal_dir == 2) { // new direction is S
+				new_search_index.x = start_point.x;
+				new_search_index.y = start_point.y + 1;
+			}
+			else {  // new direction is E
+				new_search_index.x = start_point.x + 1;
+				new_search_index.y = start_point.y;
+			}
+
+			wasteland = this.RecurseWasteland(landscape, new_search_index);
+		}
+		return wasteland;
+	}
 }
