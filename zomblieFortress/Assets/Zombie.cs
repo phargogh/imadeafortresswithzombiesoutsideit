450f6d60371f27c7012372cc21a56cc04fede931
Sean Francis-Lyon <seanflyon@gmail.com>
2014-12-06 01:25:48 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	zomblieFortress/zomblieFortress.userprefs

--- zomblieFortress/Assets/Zombie.cs
+++ zomblieFortress/Assets/Zombie.cs
@@ -1,15 +1,56 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections;
 
 public class Zombie : MonoBehaviour {
-
+	int x;
+	int y;
+	int targetx;
+	int targety;
+	int xmax = getboardx ();
+	int ymax = getboardy ();
+	int searchx;
+	int searchy;
+	
+	Zombie (int x, int y){
+		this.x = x;
+		this.y = y;
+		this.FindTarget();
+	}
+	
 	// Use this for initialization
 	void Start () {
-	
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
+	
+	void FindTargetLoop(){
+		
+		bool targetacquired = false;
+		
+		for (int i = 1; i <= this.xmax; i++){
+			
+			if (targetacquired){
+				break;
+			}
+			else{
+				for (int j = 1; j <= this.ymax; j++){
+					if(attackable(i,j)){
+						this.targetx = i
+							this.targety = j
+								targetacquired = true;
+						break;
+					}
+					
+					
+					
+				}
+			}
+		}
+	}
+	
 }
