d79bb850ecb02a942c24a43b40d0228bfcd7329c
James Douglass <jamesdouglassusa@gmail.com>
2014-12-06 12:20:24 -0800

    Adapting to work with all 8 directions

--- zomblieFortress/Assets/Board.cs
+++ zomblieFortress/Assets/Board.cs
@@ -81,24 +81,48 @@ public class Board : MonoBehaviour {
 		}
 
 		// determine the next place to search.
-		for (int cardinal_dir = 0; cardinal_dir < 4; cardinal_dir++){
+		// Directions:
+		//   1 | 0 | 7
+		//   - + - + -
+		//   2 | X | 6
+		//   - + - + -
+		//   3 | 4 | 5
+
+
+		for (int direction = 0; direction < 8; direction++){
 
 			Point new_search_index = new Point();
-			if (cardinal_dir == 0){ // new direction is N.
+			if (direction == 0){ // new direction is N.
 				new_search_index.x = start_point.x;
 				new_search_index.y = start_point.y - 1;
 			}
-			else if (cardinal_dir == 1) { // new direction is W
+			else if (direction == 1) { // NW
+				new_search_index.x = start_point.x - 1;
+				new_search_index.y = start_point.y - 1;
+			}
+			else if (direction == 2) { // new direction is W
 				new_search_index.x = start_point.x - 1;
 				new_search_index.y = start_point.y;
 			}
-			else if (cardinal_dir == 2) { // new direction is S
+			else if (direction == 3) { // SW
+				new_search_index.x = start_point.x - 1;
+				new_search_index.y = start_point.y + 1;
+			}
+			else if (direction == 4) { // new direction is S
 				new_search_index.x = start_point.x;
 				new_search_index.y = start_point.y + 1;
 			}
-			else {  // new direction is E
+			else if (direction == 5) { // SE
+				new_search_index.x = start_point.x + 1;
+				new_search_index.y = start_point.y + 1;
+			}
+			else if (direction == 6) {  // new direction is E
 				new_search_index.x = start_point.x + 1;
 				new_search_index.y = start_point.y;
+			}
+			else {  // NE
+				new_search_index.x = start_point.x + 1;
+				new_search_index.y = start_point.y - 1;
 			}
 
 			// Check boundary conditions.  Don't recurse there, if out of bounds.
