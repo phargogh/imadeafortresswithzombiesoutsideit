66108a833dbe081f6663a08c34ad789e1eeba686
Sean Francis-Lyon <seanflyon@gmail.com>
2014-12-06 00:27:13 -0800

    Tile prefab

--- zomblieFortress/Assets/Board.cs
+++ zomblieFortress/Assets/Board.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class Board : MonoBehaviour {
 
+	public GameObject tilePrefab;
+	static int width = 32;
+	GameObject [,] board = new GameObject[width,width];
+
 	// Use this for initialization
 	void Start () {
-	
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				//board[i,j] = GameObject.CreatePrimitive(PrimitiveType.
+			}
+		}
 	}
 	
 	// Update is called once per frame
