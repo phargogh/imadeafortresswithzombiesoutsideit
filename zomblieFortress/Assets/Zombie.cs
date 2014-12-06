139cfaeea54994c26048260f331a43033af1ee3d
4616 <dougal.michael@gmail.com>
2014-12-06 01:47:13 -0800

    Zombie movement

--- zomblieFortress/Assets/Zombie.cs
+++ zomblieFortress/Assets/Zombie.cs
@@ -10,11 +10,16 @@ public class Zombie : MonoBehaviour {
 	int ymax = getboardy ();
 	int searchx;
 	int searchy;
+	int directionx;
+	int directiony;
+	bool targetacquired
+	bool xmove;
 
 	Zombie (int x, int y){
 		this.x = x;
 		this.y = y;
 		this.FindTarget();
+		this.xmove = true; //randomize
 		}
 	
 	// Use this for initialization
@@ -28,29 +33,71 @@ public class Zombie : MonoBehaviour {
 	
 	}
 
-	void FindTargetLoop(){
+	void FindTarget(){
 
 		bool targetacquired = false;
 
 		for (int i = 1; i <= this.xmax; i++){
 		
-			if (targetacquired){
+			if (self.targetacquired){
 				break;
 			}
 			else{
 				for (int j = 1; j <= this.ymax; j++){
 					if(attackable(i,j)){
-						this.targetx = i
-						this.targety = j
-						targetacquired = true;
+						this.targetx = i;
+							this.targety = j;
+						this.targetacquired = true;
 						break;
 						}
-
-
 
 					}
 				}
 			}
+		this.DirectionUpdate()				
 		}
+
+
+	void DirectionUpdate(){
+		this.directionx = this.targetx - this.x;
+		this.directiony = this.targety - this.y;
+	}
+	void Move(){
+		if (this.x == this.targetx){
+			this.xmove = false 
+
+
+
+		if (this.xmove & this.x != this.targetx) {
+			this.xmove = false
+			if(this.directionx > 0){
+				this.x += 1;
+			}
+
+			else{
+				this.x -= 1;
+			}
+
+
+			}
+
+		else{
+			this.xmove = true
+			if(this.directiony > 0){
+				this.x += 1;
+			}
+			
+			else{
+				this.x -= 1;
+			}
+		
+		this.DirectionUpdate()
+		}
+
+		
+
+
+
+	}
 					
 	}
