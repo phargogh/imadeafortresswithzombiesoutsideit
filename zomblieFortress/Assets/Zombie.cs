7effa01b03588825826c5d0f943f5bf31026e6d7
4616 <dougal.michael@gmail.com>
2014-12-06 02:12:20 -0800

    zombies moving please

--- zomblieFortress/Assets/Zombie.cs
+++ zomblieFortress/Assets/Zombie.cs
@@ -1,4 +1,4 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class Zombie : MonoBehaviour {
@@ -10,25 +10,17 @@ public class Zombie : MonoBehaviour {
 	int ymax = getboardy ();
 	int searchx;
 	int searchy;
-<<<<<<< HEAD
 	int directionx;
 	int directiony;
 	bool targetacquired
-	bool xmove;
-
-=======
+		bool xmove;
 	
->>>>>>> origin/master
 	Zombie (int x, int y){
 		this.x = x;
 		this.y = y;
 		this.FindTarget();
-<<<<<<< HEAD
 		this.xmove = true; //randomize
-		}
-=======
 	}
->>>>>>> origin/master
 	
 	// Use this for initialization
 	void Start () {
@@ -40,53 +32,32 @@ public class Zombie : MonoBehaviour {
 	void Update () {
 		
 	}
-<<<<<<< HEAD
-
-	void FindTarget(){
-
-=======
 	
-	void FindTargetLoop(){
+	void FindTarget(){
 		
->>>>>>> origin/master
 		bool targetacquired = false;
 		
-<<<<<<< HEAD
-			if (self.targetacquired){
-=======
 		for (int i = 1; i <= this.xmax; i++){
 			
-			if (targetacquired){
->>>>>>> origin/master
+			if (self.targetacquired){
 				break;
 			}
 			else{
 				for (int j = 1; j <= this.ymax; j++){
 					if(attackable(i,j)){
-<<<<<<< HEAD
 						this.targetx = i;
-							this.targety = j;
+						this.targety = j;
 						this.targetacquired = true;
 						break;
-						}
-
-=======
-						this.targetx = i
-							this.targety = j
-								targetacquired = true;
-						break;
->>>>>>> origin/master
 					}
-					
-					
 					
 				}
 			}
-		this.DirectionUpdate()				
 		}
-<<<<<<< HEAD
-
-
+		this.DirectionUpdate()				
+	}
+	
+	
 	void DirectionUpdate(){
 		this.directionx = this.targetx - this.x;
 		this.directiony = this.targety - this.y;
@@ -94,43 +65,39 @@ public class Zombie : MonoBehaviour {
 	void Move(){
 		if (this.x == this.targetx){
 			this.xmove = false 
-
-
-
-		if (this.xmove & this.x != this.targetx) {
-			this.xmove = false
-			if(this.directionx > 0){
-				this.x += 1;
-			}
-
-			else{
-				this.x -= 1;
-			}
-
-
-			}
-
-		else{
-			this.xmove = true
-			if(this.directiony > 0){
-				this.x += 1;
+				
+				
+				
+			if (this.xmove & this.x != this.targetx) {
+				this.xmove = false
+				if(this.directionx > 0){
+					this.x += 1;
+				}
+				
+				else{
+					this.x -= 1;
+				}
+				
+				
 			}
 			
 			else{
-				this.x -= 1;
+				this.xmove = true
+				if(this.directiony > 0){
+					this.x += 1;
+				}
+				
+				else{
+					this.x -= 1;
+				}
+				
+				this.DirectionUpdate()
 			}
-		
-		this.DirectionUpdate()
+			
+			
+			
+			
+			
 		}
-
 		
-
-
-
 	}
-					
-=======
->>>>>>> origin/master
-	}
-	
-}
