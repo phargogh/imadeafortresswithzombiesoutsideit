f3a6c1b76e5a6cf547c297d9cfeeba659057066a
Sean Francis-Lyon <seanflyon@gmail.com>
2014-12-06 17:44:24 -0800

    fix bugs again

--- zomblieFortress/Assets/Board.cs
+++ zomblieFortress/Assets/Board.cs
@@ -71,24 +71,30 @@ public class Board : MonoBehaviour {
 
 	public Point worldPosToGridPoint(Vector3 worldPos) {
 		Point gridPos = new Point ();
-		gridPos.x = Mathf.RoundToInt(worldPos.x - transform.position.x) - widthx/2;
-		gridPos.y = Mathf.RoundToInt(worldPos.y - transform.position.y) - widthy/2;
+		gridPos.x = Mathf.RoundToInt(worldPos.x - transform.position.x) + widthx/2;
+		gridPos.y = Mathf.RoundToInt(worldPos.y - transform.position.y) + widthy/2;
 		return gridPos;
 	}
 
 	public bool spawnWalls(List<Point> walls, List<Point> towers, Point gridPos){
+		Debug.Log("placing walls near: " + gridPos.x + ", " + gridPos.y + " like " + walls[0].x);
+		List<Point> wallsToPlace = new List<Point> ();
 		foreach (Point w in walls) {
-			if (boardwall[w.x,w.y] == null){
-				Vector3 pos = new Vector3(w.x - Board.widthx/2, w.y - Board.widthy/2, 0);
-				GameObject wall = (GameObject) Instantiate(wallFab, pos, Quaternion.identity);
-				boardwall[w.x, w.y] = wall;
-				this.walls.Add(wall.GetComponent<Wall>());
+			Point p = new Point(gridPos.x + w.x, gridPos.y + w.y);
+			if (p.x >= 0 && p.x < widthx && p.y >= 0 && p.y < widthy && boardwall[p.x,p.y] == null){
+				wallsToPlace.Add(p);
 			}
 			else{
-				Debug.Log("Tried to place a wall on an occupied space: " + w.x + ", " + w.y );
 				return false;
 			}
 		}
+		foreach (Point p in wallsToPlace) {
+			Vector3 pos = new Vector3(p.x - Board.widthx/2, p.y - Board.widthy/2, 0);
+			GameObject wall = (GameObject) Instantiate(wallFab, pos, Quaternion.identity);
+			boardwall[p.x, p.y] = wall;
+			this.walls.Add(wall.GetComponent<Wall>());
+		}
+		
 		return true;
 	}
 
