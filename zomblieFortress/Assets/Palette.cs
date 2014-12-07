5e524121adff362bf4af630aaa41af6bb188d077
andrewdblevins <andrewdblevins@gmail.com>
2014-12-06 20:14:21 -0800

    added palette of colors. please use colors from the Palette when assigning color.

--- zomblieFortress/Assets/Palette.cs
+++ zomblieFortress/Assets/Palette.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Palette : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class Palette{
+	public static Color DarkBrown = new Color32(106,96,83,255);
+	public static Color Green = new Color32(168,176,147,255);
+	public static Color Rust = new Color32(199,156,126,255);
+	public static Color Brown = new Color32(146,133,99,255);
+	public static Color Red = new Color32(190,161,154,255);
+	public static Color Grey = new Color32(139,138,127,255);
+	public static Color Tan = new Color32(187,169,139,255);
 }
